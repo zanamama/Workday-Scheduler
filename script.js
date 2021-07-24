d72d37851b4fd59dcea63bb941f2936c6a562b7f
zanama <57731386+zanmama@users.noreply.github.com>
2021-07-24 19:07:07 -0400

    added commentary - still issue with save button

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 //moment().format("MMM Do YY"); 
 var today = moment();
+//var description = 
 $("#currentDay").text(today.format("MMM Do, YYYY"));
+
+//function to make event savable when clicking save in scheduler.
+var saveBtn = $('saveBtn');
+// adding an event listener for the content box of description-text. Need event to HIDE "Enter Text Here" when clicked into, so that user can begin typing into empty box.
+var description {
+    display: hide;
+  }addEventListener('click', () => {
+    if
+
+})
 
