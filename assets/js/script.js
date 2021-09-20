511662ea7e06e0f25c5f990e480751ab9c3a9c6b
zanama <57731386+zanmama@users.noreply.github.com>
2021-09-19 21:40:31 -0400

    reset styles.css and rediid script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,128 +1,78 @@
-// //moment().format("MMM Do YY");
-// var today = moment();
-// //var description =
-// $("#currentDay").text(today.format("MMM Do, YYYY"));
+//DEPENDENCIES
 
-// //function to make event savable when clicking save in scheduler.
-// var saveBtn = $("saveBtn");
-// // adding an event listener for the content box of description-text. Need event to HIDE "Enter Text Here" when clicked into, so that user can begin typing into empty box.
-// // var description {
-// //     display: hide;
-// //   } addEventListener('click', () => {
-// //     if
+var currentDateHTML = $("#current-date");
+var savedEntries = "Try Here";
+var container = $(".container");
+var schedulerTimeTags = $(".input-group-text");
 
-// // })
+//DATA with momentjs below
 
-// //listens for a click on save button
-// $(".save-button").on("click", saveTask);
-// //action added to save button
-// timeBlockSave.on("click", saveTask);
+var today = moment();
+var currentDate = today.format("MMM Do, YYYY");
 
-//===============================================
-function displayCurrentDay() {
-  let initMoment = moment().format("dddd Do, MMMM YYYY");
-  $("#currentDay").text(initMoment);
-}
+//FUNCTIONS
 
-// // Create a timeblock
-function createTimeBlock(theTime) {
-  // Create
-  let timeBlockRow = $("<div class='row'>");
-  let timeBlockHourCol = $(
-    "<div class='col-1 hour d-flex justify-content-end'>"
-  );
-  let timeBlockHour = $("<span class='hour-area'></span>");
-  let timeBlockTextareaCol = $("<div class='col-10 past px-0'>");
-  let timeBlockTextarea = $(
-    "<textarea name='' class='task-area description todo-block text-dark' id='' rows='4'></textarea>"
-  );
-  let timeBlockSaveCol = $("<div class='col-1 save-btn d-flex'>");
-  let timeBlockSave = $("<i class='fas fa-save m-auto fa-lg save-button'></i>");
+function colorCodeEntries() {
+  var time = $(this)[0].innerText.toLowerCase();
+  time = moment(time, "HERE");
+  todaysTime = moment(today.format("HERE"), "HERE");
+  var colorClass = $(this).parent().siblings()[0].className;
 
-  // 	// Add classes based on current hour
-
-  let militaryTime = parseInt(moment().format("HH"));
-  theTime = parseInt(theTime);
-
-  if (theTime === militaryTime) {
-    timeBlockTextarea.addClass("present");
-  } else if (theTime > militaryTime) {
-    timeBlockTextarea.addClass("future");
+  if (time.isBefore(today.format("HERE"))) {
+    $(this).parent().siblings()[0].className = colorClass + " past";
+  } else if (time.isAfter(todaysTime)) {
+    $(this).parent().siblings()[0].className = colorClass + " future";
   } else {
-    // Not relevant as we already defaulted the class to past
-    timeBlockTextarea.addClass("past");
+    $(this).parent().siblings()[0].className = colorClass + " present";
   }
-
-  // 	// Build
-  let timeText = `${theTime > 11 ? `${theTime} PM` : `${theTime} AM`}`;
-  timeBlockHourCol = timeBlockHourCol.append(timeBlockHour).text(timeText);
-  timeBlockTextareaCol = timeBlockTextareaCol.append(timeBlockTextarea);
-  timeBlockSaveCol = timeBlockSaveCol.append(timeBlockSave);
-
-  // 	// Retrieve todos
-  let retrievedTodo = fetchTodo(timeText);
-  timeBlockTextarea.val(fetchTodo(timeText));
-
-  // Apply a light blue border color to notify user of a saved todo
-  if (retrievedTodo) {
-    timeBlockTextarea.addClass("border border-5 border-info");
-  }
-
-  // Place
-  timeBlockRow.append(timeBlockHourCol);
-  timeBlockRow.append(timeBlockTextareaCol);
-  timeBlockRow.append(timeBlockSaveCol);
-  $(".container").append(timeBlockRow);
 }
 
-// Display work hours
-function workingHours() {
-  // create time blocks
-  for (let i = 9; i <= 17; i++) {
-    createTimeBlock(i);
+function loadSavedEntries() {
+  savedEntries = JSON.parse(localStorage.getItem("savedEntries"));
+
+  if (savedEntries) {
+    for (var i = 0; i < savedEntries.length; i++) {
+      var currEntry = savedEntries[i];
+
+      if (currEntry.date == currentDate) {
+        console.log(`.${currEntry.time}`.toLowerCase());
+        $(`#${currEntry.time}`.toLowerCase())
+          .parent()
+          .siblings()
+          .text(currEntry.entry);
+      }
+    }
+  } else {
+    savedEntries = [];
   }
 
-  // 	// listen for clicks on save buttons
-  $(".save-button").on("click", saveTask);
+  return savedEntries;
 }
 
-// Save task to local storage
-function saveTask() {
-  let textAreaVal = $(this).parent().parent().find(".todo-block").val();
-  let hourVal = $(this).parent().parent().find(".hour").text();
+function saveEntry(event) {
+  event.preventDefault();
 
-  // Apply a green border color to let user no todo is now saved
-  $(this)
+  var entry = $(this).parent().parent()[0][0].value;
+  var entryTime = $(this)
     .parent()
-    .parent()
-    .find(".todo-block")
-    .addClass("border border-success");
+    .siblings()
+    .children()
+    .children()[0] //get to the right place. start @ 0
+    .innerText.toUpperCase();
 
-  // 	// Store in localstorage
-  localStorage.setItem(`${hourVal}`, `${textAreaVal}`);
+  var savedEntry = {
+    date: currentDate,
+    time: entryTime,
+    entry: entry,
+  };
+
+  savedEntries.push(savedEntry);
 }
 
-// // Fetch stored todo
-function fetchTodo(hour) {
-  // 	// Update the textarea
-  let storedTextAreaVal = localStorage.getItem(`${hour}`);
-  return `${storedTextAreaVal ? storedTextAreaVal : ""}`;
-}
+//INITIALIZATION
 
-// // Event deligation example
+currentDateHTML.text(currentDate);
+schedulerTimeTags.each(colorCodeEntries);
+savedEntries = loadSavedEntries();
 
-// // INITIALIZATION ==========================================
-displayCurrentDay();
-workingHours();
-// ## 2021 GitHub, Inc By Zana Mathuthu.
-// Terms
-// Privacy
-// Security
-// Status
-// Docs
-// Contact GitHub
-// Pricing
-// API
-// Training
-// Blog
-// About
+container.on("click", ".saveBtn", saveEntry);
